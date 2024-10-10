2a4f3e1c577efb26f5e95f10f488d290040b400e
365cent <74791570+365cent@users.noreply.github.com>
2024-10-10 14:44:34 -0400

    fix: empty repo

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Reviews } from "@/components/reviews";
-import { getProduct } from "@/lib/sample-data";
-
-export default async function Home() {
-  const product = await getProduct("mower");
-
-  return <Reviews product={product} />;
-}
