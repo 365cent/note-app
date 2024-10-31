f9c1f986816db8670fb1235a80eba87ae58e6d29
365cent <74791570+365cent@users.noreply.github.com>
2024-10-31 16:44:29 -0400

    fix: mobile page responsible

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
 
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-			<header className="hidden items-center justify-between row-start-1 w-full sm:flex">
+			<header className="flex items-center justify-between row-start-1 w-full">
 				<div className="flex items-center gap-4">
 					<Image
 						className="dark:invert"
@@ -33,7 +33,7 @@ export default function Home() {
 						priority
 					/>
 					<h1 className="text-xl font-semibold">Note.lat</h1>
-					<span>Lecture note has never been so easy</span>
+					<span className="hidden sm:block">Lecture note has never been so easy</span>
 				</div>
 				<nav>
 					<Button className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white">
