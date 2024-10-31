0abb8ae1fa8d4179952163b4077f5667a2370e5c
365cent <74791570+365cent@users.noreply.github.com>
2024-10-31 14:51:58 -0400

    chore: add test request button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
+"use client";
 import Image from "next/image";
 
 export default function Home() {
+	const handleClick = () => {
+		// send json to https://neo4j-backend-three.vercel.app/api/addUser
+		fetch("https://neo4j-backend-three.vercel.app/api/addUser", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				message: "This is a test note",
+			}),
+		})
+	}
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -29,7 +42,7 @@ export default function Home() {
 				<div className="flex gap-4 items-center flex-col sm:flex-row">
 					<button
 						className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-						
+						onClick={handleClick}
 					>
 						<Image
 							className="dark:invert"
@@ -38,7 +51,7 @@ export default function Home() {
 							width={20}
 							height={20}
 						/>
-						Deploy now
+						Test Request
 					</button>
 					<a
 						className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
