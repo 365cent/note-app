ffc841457a1222a8d9812a4d3d4fbfb607787984
365cent <74791570+365cent@users.noreply.github.com>
2024-10-31 20:31:06 -0400

    fix: update webpack memory optimizations configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  "experimental.webpackMemoryOptimizations": true
+  experimental: {
+    webpackMemoryOptimizations: true,
+  },
 };
 
 export default nextConfig;
