9d050130526ab1c89fd97a6e8231e523133e8037
365cent <74791570+365cent@users.noreply.github.com>
2024-11-07 01:29:05 -0500

    feat: create new page for note creation with Header and Recording components

---
+++ src/app/create/page.tsx
@@ -0,0 +1,14 @@
+import Header from "@/app/components/header";
+import Recording from "@/app/components/recording";
+
+export default function Create() {
+    return (
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <Header />
+            <main className="mx-auto max-w-screen-sm w-full">
+                <h1 className="text-3xl font-semibold mx-auto">Create Note</h1>
+                <Recording />
+            </main>
+        </div>
+    );
+}
