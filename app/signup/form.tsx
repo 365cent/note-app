fc404fbc223dde65d612e0eacb483d2d2840b1a0
365cent <74791570+365cent@users.noreply.github.com>
2024-11-14 21:43:14 -0500

    feat: fetch university list from API and update university selection in signup form

--- app/signup/form.tsx
+++ app/signup/form.tsx
@@ -3,30 +3,29 @@
 import { useState } from 'react'
 import { Combobox, ComboboxButton, ComboboxInput, ComboboxOption, ComboboxOptions, Field, Fieldset, Input, Label, Legend } from '@headlessui/react'
 
-const universities = [
-    { domain: 'harvard.edu', name: 'Harvard University' },
-    { domain: 'stanford.edu', name: 'Stanford University' },
-    { domain: 'mit.edu', name: 'Massachusetts Institute of Technology' },
-    { domain: 'berkeley.edu', name: 'University of California, Berkeley' },
-    { domain: 'ox.ac.uk', name: 'University of Oxford' },
-    { domain: 'cam.ac.uk', name: 'University of Cambridge' },
-    { domain: 'caltech.edu', name: 'California Institute of Technology' },
-    { domain: 'yale.edu', name: 'Yale University' },
-    { domain: 'princeton.edu', name: 'Princeton University' },
-    { domain: 'uchicago.edu', name: 'University of Chicago' },
-    { domain: 'uwo.ca', name: 'Western University' },
-]
+
 
 export default function SignForm() {
-    const [query, setQuery] = useState('')
-    const [selectedUniversity, setSelectedUniversity] = useState(universities[0])
+    const [query, setQuery] = useState('');
+    const [selectedUniversity, setSelectedUniversity] = useState<{ domain: string; name: string } | null>(null);
+    const [universities, setUniversities] = useState<{ domain: string; name: string }[]>([]);
 
     const filteredUniversities =
         query === ''
             ? universities
             : universities.filter((university) => {
                 return university.name.toLowerCase().includes(query.toLowerCase())
-            })
+            });
+
+    const fetchUniversityList = async () => {
+        const response = await fetch('https://dash.note.lat/api/getAllUni', {
+            method: 'GET'
+        });
+        const json = await response.json();
+        const { data } = json;
+        console.log(data);
+        setUniversities(data);
+    };
 
     const handleSignup = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
@@ -39,7 +38,7 @@ export default function SignForm() {
                 user_name: event.currentTarget.username.value,
                 user_email: event.currentTarget.email.value,
                 user_password: event.currentTarget.password.value,
-                uni_name: selectedUniversity,
+                uni_name: selectedUniversity?.name,
             }),
         })
     }
@@ -75,15 +74,16 @@ export default function SignForm() {
                 <Field>
                     <Label htmlFor="university" className="text-sm/6 font-medium text-black">University</Label>
                     <div className="relative">
-                        <Combobox value={selectedUniversity} onChange={(value) => setSelectedUniversity(value ?? universities[0])}>
+                        <Combobox value={selectedUniversity} onChange={(value) => setSelectedUniversity(value)}>
                             <div className="relative">
                                 <ComboboxInput
                                     className="w-full rounded-lg border-none bg-black/5 py-1.5 pr-8 pl-3 text-sm/6 text-black focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-black/25"
                                     displayValue={(university: { name: string }) => university?.name}
                                     onChange={(event) => setQuery(event.target.value)}
+                                    placeholder="Search for your university"
+                                    onFocus={fetchUniversityList}
                                 />
                                 <ComboboxButton className="absolute inset-y-0 right-0 flex items-center pr-2">
-                                    {/* <ChevronDown className="h-5 w-5 text-gray-400" aria-hidden="true" /> */}
                                     <i className="ri-arrow-down-s-line text-gray-400"></i>
                                 </ComboboxButton>
                             </div>
@@ -92,28 +92,22 @@ export default function SignForm() {
                                     <ComboboxOption
                                         key={university.domain}
                                         value={university}
-                                        className={({ active }) =>
-                                            `relative cursor-default select-none py-2 pl-3 pr-9 ${active ? 'bg-black text-white' : 'text-gray-900'
-                                            }`
-                                        }
+                                        className="group flex cursor-default items-center gap-2 rounded-lg mx-1 py-1 px-2 select-none data-[focus]:bg-black/10 empty:bg-transparent"
                                     >
-                                        {({ selected, active }) => (
-                                            <>
-                                                <span className={active ? 'font-semibold' : 'font-normal'}>
-                                                    {university.name}
-                                                </span>
-                                                {selected && (
-                                                    <span
-                                                        className={`${active ? 'text-white' : 'text-black'
-                                                            } absolute inset-y-0 left-0 flex items-center pl-3`}
-                                                    >
-                                                        <i className='ri-check-line'></i>
-                                                    </span>
-                                                )}
-                                            </>
-                                        )}
+                                        <i className="invisible size-4 fill-white group-data-[selected]:visible ri-check-line"></i>
+                                        <div className="text-sm/6 text-gray-900">{university.name}</div>
                                     </ComboboxOption>
                                 ))}
+                                {filteredUniversities.length === 0 && (
+                                    <ComboboxOption
+                                        key=""
+                                        value=""
+                                        className="group flex cursor-default items-center gap-2 rounded-lg mx-1 py-1 px-2 select-none data-[focus]:bg-black/10 empty:bg-transparent"
+                                    >
+                                        <i className="invisible size-4 fill-white group-data-[selected]:visible ri-check-line"></i>
+                                        <div className="text-sm/6 text-gray-900">No university found</div>
+                                    </ComboboxOption>
+                                )}
                             </ComboboxOptions>
                         </Combobox>
                     </div>
